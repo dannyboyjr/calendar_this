38267f9cd80e4ad64b6b5be08fe92a93ce27a865
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-02-01 18:23:05 -0700

    updated with blueprint and routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
+from app import routes
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app.config.update({"SECRET_KEY": os.environ.get("SECRET_KEY")})
 
-@app.route('/')
-def index():
-    return '<h1>Getting started</h1>'
+app.register_blueprint(routes.bp)
