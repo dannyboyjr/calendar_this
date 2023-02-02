24f724d5ad02922add99d13dddef0b5f4616e9e8
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-02-01 19:09:32 -0700

    added sqlite3 and first appointment

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from app import routes
 from flask import Flask
-import os
+from app.config import Config
 
 app = Flask(__name__)
-app.config.update({"SECRET_KEY": os.environ.get("SECRET_KEY")})
+
+app.config.from_object(Config)
 
 app.register_blueprint(routes.bp)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,31 @@
 from flask import (Blueprint, render_template)
+import os
+import sqlite3
+from datetime import datetime
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+DB_FILE = os.environ.get("DB_FILE")
+
+
 @bp.route("/")
 def main():
-    return render_template('main.html')
+    with sqlite3.connect(DB_FILE) as conn:
+        curs = conn.cursor()
+        curs.execute(
+            """
+            SELECT id, name, start_datetime, end_datetime
+            FROM appointments
+            ORDER BY start_datetime
+            """
+        )
+        results = curs.fetchall()
+        rows = []
+        for item in results:
+            item = list(item)
+            item[2] = datetime.strptime(item[2], '%Y-%m-%d %H:%M:%S')
+            item[3] = datetime.strptime(item[3], '%Y-%m-%d %H:%M:%S')
+            rows.append(item)
+    return render_template('main.html', rows=rows)
+
+
