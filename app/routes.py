45f462813150bf012d53b112a5d637749f9b0788
Dan Kimball <dankimball@Dans-MacBook-Pro.local>
2023-02-01 18:28:35 -0700

    added Ninja 2 & Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import (Blueprint, render_template)
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route("/")
 def main():
-    return "<h1>Calendar Working</h1>"
+    return render_template('main.html')
